f3f4d805b0d096e0c462df79e0a423890fc97f0a
Anthony Tesoriero <anttesoriero@gmail.com>
2019-11-04 15:32:20 -0500

    Update FizzBuzz.java

--- FizzBuzz.java
+++ FizzBuzz.java
@@ -22,12 +22,12 @@ public class FizzBuzz
 			// FizzBuzz
 			for(int i = 1; i <= n; i++)
 			{
-			    if(i % 3 == 0)
+			    if(i % 15 == 0)
+				System.out.println("FizzBuzz");
+			    else if(i % 3 == 0)
 			    	System.out.println("Fizz");	
 			    else if(i % 5 == 0)
 			    	System.out.println("Buzz");
-			    else if(i % 15 == 0)
-				System.out.println("FizzBuzz");
 			    else
 			    	System.out.println(i);
 			}
