3d8db88cbb8d1154978fa76b65ff9711262340ea
Anthony Tesoriero <anttesoriero@gmail.com>
2019-11-04 09:58:10 -0500

    Update FizzBuzz.py

--- FizzBuzz.py
+++ FizzBuzz.py
@@ -1,18 +1,9 @@
 # Anthony Tesoriero, FizzBuzz
-# Print numbers from 1 to selected n
+# Print numbers from 1 to 100
 # For multiples of 3, print "Fizz" instead
 # For multiples of 5, print "Buzz" instead
 
-# Error Handing
-while True:
-	try:
-		n = int(input("Choose an integer: "))
-		break
-	except ValueError:
-		print("Error: Not an integer, try again.")
-
-# FizzBuzz
-for i in range(1, n+1):
+for i in range(1, 101):
 	if i % 3 == 0:
 		print("Fizz")
 	elif i % 5 == 0:
