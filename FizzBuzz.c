05eb17bf93ab3c66a99bb89add96bd616df7afd5
Anthony Tesoriero <anttesoriero@gmail.com>
2019-11-04 15:31:32 -0500

    Update FizzBuzz.c

--- FizzBuzz.c
+++ FizzBuzz.c
@@ -20,12 +20,12 @@ int main(void)
     int i;
     for(i = 1; i <= n; i++)
     {
-        if(i % 3 == 0)
+        if(i % 15 == 0)
+            printf("FizzBuzz\n");
+        else if(i % 3 == 0)
         	printf("Fizz\n");
         else if(i % 5 == 0)
         	printf("Buzz\n");
-        else if(i % 15 == 0)
-            printf("FizzBuzz\n");
         else
         	printf("%d\n", i);
     }
