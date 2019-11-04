0fb140f45bd19f2548f8d1d7542659f6d58e5da1
Anthony Tesoriero <anttesoriero@gmail.com>
2019-11-04 10:26:51 -0500

    Create FizzBuzz.c

---
+++ FizzBuzz.c
@@ -0,0 +1,31 @@
+/*
+Anthony Tesoriero, FizzBuzz
+
+Print numbers from 1 to selected n
+For multiples of 3, print "Fizz" instead
+For multiples of 5, print "Buzz" instead
+*/
+
+#include <stdio.h>
+
+int main(void) 
+{
+    // User Selection
+    int n;
+    printf("Enter an integer: ");
+    scanf("%d", &n);
+    
+    // FizzBuzz
+    int i;
+    for(i = 1; i <= n; i++)
+    {
+        if(i % 3 == 0)
+        	printf("Fizz\n");
+        else if(i % 5 == 0)
+        	printf("Buzz\n");
+        else
+        	printf("%d\n", i);
+    }
+    
+    return 0;
+}
